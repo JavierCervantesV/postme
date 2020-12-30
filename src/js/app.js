0a58c95168abd799cd5a0f15edf493e3addb9895
Javier Cervantes <javiercervantesv@hotmail.com>
2020-12-29 23:49:58 -0600

    Invitación a instalar

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ let MAIN;
 let MODAL_POST;
 let BTN_SHOW_POST;
 let BTN_CANCEL_POST;
-
+let deferredPrompt;
 // Funciones
 const showPostModal = () => {
   MAIN.style.display = 'none';
@@ -16,6 +16,12 @@ const closePostModal = () => {
   MAIN.style.display = 'block';
   MODAL_POST.style.transform = 'translateY(100vh)';
 };
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log("Anulando ---------------------------");
+  e.preventDefault();
+  deferredPrompt = e;
+});
 
 // Cuando se cargue todo nuestro DOM
 window.addEventListener('load', async () => {
